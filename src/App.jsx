469c8f0ecf7e6e65f64778ab1bc0be9c237307fe
nihalcool123 <nihalmah123@gmail.com>
2024-10-25 10:53:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MemoryGame from './components/MemoryGame';
+
+function App() {
+  return (
+    <>
+      <div>
+        <MemoryGame />
+      </div>
+    </>
+  );
+}
+
+export default App;
